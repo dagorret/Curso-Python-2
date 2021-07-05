73961099cd3ba2793a2913751baca4241e733a3d
Carlos Dagorret <dagorret@eco.unrc.edu.ar>
2021-07-05 19:33:08 -0300

    operadores

---
+++ p3_5_operadores.py
@@ -0,0 +1,20 @@
+print("Suma de 1 + 1")
+print(1 + 1)
+
+print("Resta de 2 - 1")
+print(2 - 1)
+
+print("Multiplicación de 6 * 2")
+print(6 * 2)
+
+print("De divicion de 24 / 4")
+print (24 / 4)
+
+print("Modulo de 10 % 3")
+print (10 % 3)
+
+print("División entera de 10 // 3")
+print (10 // 3)
+
+print(4 ** 2)
+
