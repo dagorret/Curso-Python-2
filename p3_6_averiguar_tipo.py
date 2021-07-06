854d47a9e5ef5ef828709efff74a792ec703ea96
Carlos Dagorret <dagorret@eco.unrc.edu.ar>
2021-07-05 22:05:38 -0300

    Tipo de datos

---
+++ p3_6_averiguar_tipo.py
@@ -0,0 +1,14 @@
+nombre = "Carlos"
+
+numero = 5
+
+decimal =  8.1
+
+print("Tipo de Cadena")
+print(type(nombre))
+
+print("Tipo Entero")
+print(type(numero))
+
+print("Tipo Float")
+print(type(decimal))
